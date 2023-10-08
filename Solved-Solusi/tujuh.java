7d93b6dc72a7db221981e836be3acc75765d9f87
Galih Muhammad Ichsan <nyomangalih12@gmail.com>
2023-10-08 08:12:15 +0700

    Add files via upload

---
+++ Solved-Solusi/tujuh.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class tujuh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Tinggal menambahkan item yang ingin di cleaner
+        String[] cleaner = {",","\'","\"","@","/","&","(",")"};
+        System.out.println("Masukkan Kalimat yang ingin dibersihkan !!");
+        String input = scanner.nextLine();
+        System.out.println("Sebelum : " + input);
+
+        /*//Terlalu Panjang
+        // Menghapus ,
+        String output = input.replace(",", "");
+        // Menghapus ' kutip satu
+        output = output.replace("\'", "");
+        // Menghapus " kutip dua
+        output = output.replace("\"", "");
+        // Menghapus @ anotasi
+        output = output.replace("@", "");
+        // Menghapus / garis miring
+        output = output.replace("/", "");
+        // Menghapus & dan
+        output = output.replace("&", "");
+         System.out.println("Sesudah : " + output); */
+
+        //Menggunakan Looping agar code lebih sederhana
+        String output = input;
+        for(int i=0; i<cleaner.length; i++){
+            output = output.replace(cleaner[i], " ");
+        }
+        System.out.println("Sesudah : "+ output);
+
+    }
+    
+}
